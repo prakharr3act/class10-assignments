38f6925ace863fbe32cdee900207a56ea2816009
Prakharr3act <prakhar.codes433@gmail.com>
2026-05-01 20:37:43 +0530

    add whatsapp group

---
+++ script.js
@@ -0,0 +1,5 @@
+WhatsappBtn = document.getElementById("whts-bnnr");
+
+WhatsappBtn.addEventListener("click", function() {
+   window.location.href = "https://chat.whatsapp.com/LBqZN6ZRNbCF1Puy7E70Tc";
+});
